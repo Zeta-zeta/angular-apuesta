912d474b42c67bd631905ef7a32603f08515207f
AndreHC06 <sm62949039@idat.pe>
2024-12-01 00:14:21 -0500

    Listado de partidos por filtrado de equipo

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { InicioService } from '../../services/inicio.service';
-import { Partido } from '../../interfaces/partido.interface';
+import { Equipo, Partido } from '../../interfaces/partido.interface';
 import { ComunicacionService } from '../../services/comunicacion.service';
 import { UsuarioService } from '../../services/usuario.service';
+import { EquiposService } from '../../services/equipos.service';
+import { PartidosService } from '../../services/partidos.service';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
@@ -14,21 +16,56 @@ export class InicioComponent implements OnInit{
   partidosEnVivo: Partido[] = [];
   partidosProximos: Partido[] = [];
   partidosTerminados: Partido[] = [];
-
+  listaEquipos: Equipo[] = []
   constructor(
     private inicioService: InicioService,
     private comunicacionService:ComunicacionService,
     private usuarioService: UsuarioService,
+    private equipoService: EquiposService,
+    private partidoService: PartidosService
+
   ){}
 
   async ngOnInit(): Promise<void> {
     this.getPartidos();
+    this.getEquipos();
+
     const usuarioJSON = localStorage.getItem('usuario');
           if (usuarioJSON) {
             const usuarioOldStorage = JSON.parse(usuarioJSON);
             await this.usuarioService.actualizarLocalStorageConDatosDelAPI(usuarioOldStorage.id);
             this.comunicacionService.emitirFondosActualizados();
           }
+  }
+  filteredEquipos: Equipo[] = [];
+
+  filterEquipos(event: Event): void|Equipo {
+    const input = event.target as HTMLInputElement; // Asegura que es un HTMLInputElement
+    const query = input.value; // Accede al valor
+    if (query.trim() === '') {
+      this.filteredEquipos = [];
+      return;
+    }
+    this.filteredEquipos = this.listaEquipos.filter(equipo =>
+      equipo.nombre.toLowerCase().includes(query.toLowerCase())
+    ); 
+    return;
+   }
+   
+   selectEquipo(equipo: Equipo): void {
+    this.partidosEnVivo = this.listaPartidos.filter(
+      partido =>
+        partido.idEquipoLocal.id === equipo.id && partido.estado === "En vivo" || partido.idEquipoVisitante.id === equipo.id && partido.estado === "En vivo"
+    );
+    this.partidosProximos = this.listaPartidos.filter(
+      partido =>
+        partido.idEquipoLocal.id === equipo.id && partido.estado === "Proximo" || partido.idEquipoVisitante.id === equipo.id && partido.estado === "Proximo"
+    );
+    this.partidosTerminados = this.listaPartidos.filter(
+      partido =>
+        partido.idEquipoLocal.id === equipo.id && partido.estado === "Terminado" || partido.idEquipoVisitante.id === equipo.id && partido.estado === "Terminado"
+    );
+  
   }
 
 
@@ -42,6 +79,17 @@ export class InicioComponent implements OnInit{
         console.log(err)
       }
     });
+  }
+  getEquipos(): void{
+    this.equipoService.getEquipos().subscribe({
+      next: (result) =>{
+        this.listaEquipos = result;
+      },
+      error:(err)=>{
+        console.log(err)
+      }
+    });
+
   }
 
   filtrarPartidos(): void {
